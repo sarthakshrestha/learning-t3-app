bb62887b36eda74fe346014f5bd0057f711cf46c
Sarthak Shrestha <sarthakstha24@gmail.com>
2024-07-07 22:36:03 +0545

    Linked to DB

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,23 @@
+import { getOrderByOperators } from "drizzle-orm";
 import { headers } from "next/headers";
 import Link from "next/link";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/8b034d5e-6e7a-45a4-b4c2-e641f672b1e8-eap3ia.JPG",
-  "https://utfs.io/f/0b89fa39-e242-4e0e-a0df-ef1c0d281300-eap3ju.JPG",
-  "https://utfs.io/f/5483f33c-dd3e-4875-83df-f60a0ac6d42f-eaqxg7.JPG",
-  "https://utfs.io/f/2dc2ab77-d746-49f1-b62e-f5c604d07996-eaqxi2.JPG",
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
-
 export default async function HomePage() {
   headers();
-  const posts = await db.query.posts.findMany();
-  console.log(posts);
+  const images = await db.query.posts.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-        {mockImages.map((image) => (
-          <div key={image.id} className="w-48">
+        {images.map((image) => (
+          <div key={image.id} className="flex w-48 flex-col">
             <img src={image.url} alt="image" />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -22,15 +22,16 @@ export const posts = createTable(
   "post",
   {
     id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
+    name: varchar("name", { length: 256 }).notNull(),
+    url: varchar("url", { length: 1024 }).notNull(),
     createdAt: timestamp("created_at", { withTimezone: true })
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
     updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date()
+      () => new Date(),
     ),
   },
   (example) => ({
     nameIndex: index("name_idx").on(example.name),
-  })
+  }),
 );
