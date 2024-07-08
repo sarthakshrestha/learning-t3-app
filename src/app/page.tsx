b535b2df7c3baf3d105a0b8cc9596c11f5a87fee
Sarthak Shrestha <sarthakstha24@gmail.com>
2024-07-08 23:44:12 +0545

    Used Next Images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { db } from "~/server/db";
 import { images } from "~/server/db/schema";
 import { getMyImages } from "~/server/queries";
+import Image from "next/image";
 
 export const dynamic = "force-dynamic";
 
@@ -15,10 +16,18 @@ export default async function HomePage() {
     const images = await getMyImages();
 
     return (
-      <div className="flex flex-wrap gap-4 p-9">
+      <div className="flex flex-wrap justify-center gap-4 p-9">
         {images.map((image) => (
-          <div key={image.id} className="flex w-48 flex-col">
-            <img src={image.url} />
+          <div key={image.id} className="flex h-48 w-48 flex-col">
+            <Image
+              src={image.url}
+              style={{
+                objectFit: "contain",
+              }}
+              width={480}
+              height={480}
+              alt={image.name}
+            />
             <div>{image.name}</div>
           </div>
         ))}
